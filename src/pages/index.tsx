308cc918778d73d919b15b30df5421d93ef0f8dd
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2023-05-02 00:29:57 +0900

    add Components in MainPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import Head from 'next/head'
+import CardList from '../components/CardList'
+import Card from '../components/Card'
+import ButtonList from '../components/ButtonList'
+import Button from '../components/Button'
 
 export default function Home() {
   return (
@@ -7,7 +11,22 @@ export default function Home() {
         <title>baro escape room</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main>
+      <main className="p-5">
+        <h1 className="font-bold text-3xl">바로방탈출</h1>
+        <div className="my-5">
+          <ButtonList>
+            <Button text="오늘" />
+            <Button text="내일" />
+            <Button text="이번주" />
+          </ButtonList>
+        </div>
+        <div>
+          <CardList>
+            <Card />
+            <Card />
+            <Card />
+          </CardList>
+        </div>
       </main>
     </>
   )
