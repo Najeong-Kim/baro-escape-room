af2794c2362b3d1e36cc97debd108987442baa48
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2023-05-02 10:19:56 +0900

    update Card props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,22 +1,32 @@
-export default function Card() {
+interface Props {
+  themeName: string;
+  date: string;
+  time: string;
+  isBooked: boolean;
+  cafeName: string;
+}
+
+export default function Card({themeName, date, time, isBooked, cafeName}: Props) {
   return (
     <div className="p-6 bg-gray-50 shadow-lg rounded-md w-56 h-64">
-      <h1 className="text-lg font-semibold text-slate-900">
-        방탈출 테마명
+      <h1 className="text-lg font-semibold text-slate-900 w-44">
+        {themeName}
       </h1>
       <div className="text-sm font-medium text-slate-600">
-        날짜
+        {date}
       </div>
       <div className="text-sm font-medium text-slate-700">
-        시간
+        {time}
       </div>
       <div className="my-6 border-b border-slate-200">
       </div>
-      <button className="w-full h-10 mb-6 text-sm font-semibold rounded-md bg-black text-white">
+      {isBooked ? <button className="w-full h-10 mb-6 text-sm font-semibold rounded-md bg-slate-300">
+        예약 불가
+      </button> : <button className="w-full h-10 mb-6 text-sm font-semibold rounded-md bg-black text-white">
         예약 가능
-      </button>
+      </button>}
       <p className="text-sm text-slate-700">
-        방탈출 카페명
+        {cafeName}
       </p>
     </div>
   )
