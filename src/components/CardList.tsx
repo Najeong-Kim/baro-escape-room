d16312d7361f002c7cf0c27bcce10f49385ba7ee
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2023-05-06 14:47:41 +0900

    add isLoading

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Card from './Card'
+import Spinner from './Spinner'
 
 interface ThemeList {
   [key: string]: {
@@ -13,17 +14,21 @@ interface ThemeList {
 
 export default function CardList() {
   const [themeList, setThemeList] = useState<ThemeList>({})
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   useEffect(() => {
     (async () => {
+      setIsLoading(true)
       const res = await fetch("/api/resource/1")
       const data = await res.json() 
       setThemeList(data)
+      setIsLoading(false)
     })()
   }, [])
 
   return (
-    <div className="flex overflow-x-auto space-x-4 w-full">
+    <div className="flex overflow-x-auto space-x-4 w-full h-72">
+      {isLoading && <div className="flex w-full h-auto justify-center items-center"><Spinner /></div>}
       {Object.entries(themeList).map(([theme, list]) => (
         list.map(({date, timeList}) => (
           timeList.map(item => (
